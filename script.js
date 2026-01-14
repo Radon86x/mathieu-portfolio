f570ea30c313ce1b07abfacc0e00645bd3284785
Radon86x <113633932+Radon86x@users.noreply.github.com>
2026-01-14 00:46:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ typewriter(typedEl, phrases);
       box-shadow: 0 18px 46px rgba(0,0,0,0.28);
     }
     .project-card.tapped .card-more {
-      max-height: 350px;
+      max-height: 600px;
       opacity: 1;
     }
     .project-card.tapped::after { opacity: 1; }
